e650bfc7cda43604d14040512a6c6f36e18b00d9
Agos <agozavia@gmail.com>
2024-10-21 10:48:36 +0200

    resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import {useState} from 'react';
 import './App.css'
 
 function App() {
- //Aquí tu código
+    let [name, newName] = useState("Sofía");
+
+    const data = () => {
+        newName(name="Data")
+    };
+    const reyes = () => {
+        newName(name="Reyes")
+    };
+    const soraya = () => {
+        newName(name="Soraya")
+    };
+
+
+    return (
+        <div>
+        <h2>Teacher Name: {name}</h2>
+        <ul>
+            <li><button onClick={data}>Data</button></li>
+            <li><button onClick={reyes}>Reyes</button></li>
+            <li><button onClick={soraya}>Soraya</button></li>
+        </ul>
+        </div>
+    );
 }
 
 export default App
